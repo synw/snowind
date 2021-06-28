200c7c68772645ff3b09799612b1279d695bf940
synw <synwx@protonmail.com>
2021-06-28 13:22:31 +0200

    Improve build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import typescript2 from "rollup-plugin-typescript2"
 const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    typescript2({
+      check: false,
+      tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
