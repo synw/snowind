e0cff730a5cbc04bcaecd6ad02572fcef25bc7db
synw <synwx@protonmail.com>
2021-07-03 12:00:30 +0200

    Config options

--- vite.config.ts
+++ vite.config.ts
@@ -6,24 +6,27 @@ const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
     typescript2({
       check: false,
       tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+      clean: true
     }),
+    vue(),
   ],
-  build: {
+  /*build: {
+    emptyOutDir: false,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'snowind'
     },
     rollupOptions: {
       external: ['vue'],
+      input: "src/index.ts",
       output: {
         globals: {
           vue: 'Vue'
         }
       }
     }
-  }
+  }*/
 })
