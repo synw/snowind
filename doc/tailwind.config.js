7d00861286679d4d7fe26d8c849ada2c52fa82b0
synw <synwx@protonmail.com>
2021-07-04 04:50:24 +0200

    Update doc

--- doc/tailwind.config.js
+++ doc/tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './dist/**/*.{vue,js,ts,jsx,tsx}'],
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', '../docs/**/*.{vue,js,ts,jsx,tsx}'],
   },
   darkMode: 'class',
   theme: {
