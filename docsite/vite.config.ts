aacc0690f251191bb36d686c8b1324c92a4d42ac
synw <synwx@protonmail.com>
2021-11-03 21:27:26 +0100

    Update viteconfig

--- docsite/vite.config.ts
+++ docsite/vite.config.ts
@@ -1,7 +1,12 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import typescript2 from "rollup-plugin-typescript2"
-const path = require('path')
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
+//const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +17,14 @@ export default defineConfig({
       clean: true
     }),
     vue(),
+    Components({
+      resolvers: IconsResolver(),
+    }),
+    Icons({
+      scale: 1.2,
+      defaultClass: 'inline-block align-middle',
+      compiler: 'vue3',
+    }),
   ],
   resolve: {
     alias: [
