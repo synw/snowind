14d972889fd9541a835ba8555a3ba8f70621ede0
synw <synwx@protonmail.com>
2021-11-10 22:54:30 +0100

    Add defaults for breakpoints in screen state

--- packages/@snowind-state/src/screen.ts
+++ packages/@snowind-state/src/screen.ts
@@ -3,7 +3,7 @@ import { TwBreakpoint } from './types';
 
 const twbreakpoints = useBreakpoints(breakpointsTailwind);
 
-export default function useScreenSize(mobileBreakpoint: TwBreakpoint, tabletBreakpoint: TwBreakpoint) {
+export default function useScreenSize(mobileBreakpoint: TwBreakpoint = "sm", tabletBreakpoint: TwBreakpoint = "lg") {
   const isMobile = twbreakpoints.smaller(mobileBreakpoint);
   const isTablet = twbreakpoints.between(mobileBreakpoint, tabletBreakpoint);
   const isDesktop = twbreakpoints.greater(tabletBreakpoint);
