5691e866f18a13380147553fe128e4fe41104c45
synw <synwx@protonmail.com>
2021-11-10 22:54:55 +0100

    Upgrade header package

--- packages/@snowind-header/src/SwHeader.vue
+++ packages/@snowind-header/src/SwHeader.vue
@@ -1,25 +1,22 @@
 <template>
   <div class="flex flex-row items-center">
-    <div class="flex-none hidden" :class="breakpoint + ':block'">
+    <div class="flex-none" v-if="!isMobile">
       <slot name="branding"></slot>
     </div>
-    <div class="inline-flex flex-row items-center h-full" :class="breakpoint + ':hidden'">
+    <div class="inline-flex flex-row items-center h-full" v-if="isMobile">
       <div @click="back()">
         <slot name="mobile-back"></slot>
       </div>
 
       <slot name="mobile-branding"></slot>
     </div>
-    <div class="flex-grow hidden h-full" :class="breakpoint + ':block'">
+    <div class="flex-grow h-full" v-if="!isMobile">
       <slot name="menu"></slot>
     </div>
-    <div
-      class="flex items-center justify-end flex-grow h-full cursor-pointer"
-      :class="breakpoint + ':hidden'"
-    >
+    <div class="flex items-center justify-end flex-grow h-full cursor-pointer" v-if="isMobile">
       <div class="border-none btn" @click="onClick()">
         <slot name="menuicon">
-          <div class="mr-5 text-3xl" :class="breakpoint + ':hidden'">
+          <div class="mr-5 text-3xl">
             <svg
               class="fill-current"
               xmlns="http://www.w3.org/2000/svg"
@@ -47,17 +44,21 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, toRefs } from "vue";
+import { useScreenSize, TwBreakpoint } from "@snowind/state";
 
 export default defineComponent({
   props: {
     breakpoint: {
-      type: String as () => "sm" | "md" | "lg" | "xl" | "2xl",
+      type: String as () => TwBreakpoint,
       default: () => "sm",
     },
   },
   emits: ["togglemenu"],
   setup(props, { emit }) {
+    const { breakpoint } = toRefs(props);
+    const { isMobile } = useScreenSize(breakpoint.value, 'lg');
+
     function onClick() {
       emit("togglemenu");
     }
@@ -71,6 +72,7 @@ export default defineComponent({
     return {
       onClick,
       back,
+      isMobile,
     };
   },
 });
