2d5d4085b2c5e54a0baeb9c28cae8d5f4c675322
synw <synwx@protonmail.com>
2022-02-05 12:44:56 +0100

    Linting

--- packages/@snowind-state/src/user/index.ts
+++ packages/@snowind-state/src/user/index.ts
@@ -1,9 +1,3 @@
-// Copyright (c) 2021 Teal.Finance
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import { useStorage } from '@vueuse/core'
 import { ref } from 'vue';
 
