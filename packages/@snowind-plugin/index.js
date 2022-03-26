fc5dde30eac2376973781475f2ed80f827866d7e
synw <synwx@protonmail.com>
2022-03-26 11:21:53 +0100

    Fix in plugin for progress stepper

--- packages/@snowind-plugin/index.js
+++ packages/@snowind-plugin/index.js
@@ -195,7 +195,7 @@ module.exports = plugin(function ({ addComponents, theme }) {
       },
       '& .stepper-line': {
         flex: '1 1 auto',
-        transitionProperty: 'color',
+        transitionProperty: 'border-color',
         transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
         transitionDuration: '500ms',
         borderTopWidth: '2px',
