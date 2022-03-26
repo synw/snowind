2410ed98967dd89078ce8a7158da7e0ab6622be7
synw <synwx@protonmail.com>
2022-03-26 11:22:38 +0100

    Add progress stepper package

---
+++ packages/@snowind-stepper/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import typescript2 from "rollup-plugin-typescript2"
+const path = require('path')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    typescript2({
+      check: false,
+      tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+      clean: true
+    }),
+    vue(),
+  ],
+  build: {
+    emptyOutDir: false,
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      name: '@snowind/stepper'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
