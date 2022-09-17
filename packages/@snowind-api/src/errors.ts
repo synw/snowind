ff78094e0c6f6433e7436ba11b7e6bb9ff5cd401
synw <synwx@protonmail.com>
2022-09-17 12:51:08 +0200

    Update api package

--- packages/@snowind-api/src/errors.ts
+++ packages/@snowind-api/src/errors.ts
@@ -1,5 +1,6 @@
 import { ResponseErrorType } from "./interfaces";
 
+/** Response error class */
 class ResponseError extends Error {
   type: ResponseErrorType;
   response: Response;
@@ -7,7 +8,7 @@ class ResponseError extends Error {
   constructor(response: Response, type: ResponseErrorType, errMsg?: string) {
     super(response.statusText);
     this.name = `ResponseError (${type})`;
-    this.stack = (new Error() as any).stack;
+    this.stack = super.stack;
     this.response = response;
     this.type = type;
     let _message = "";
