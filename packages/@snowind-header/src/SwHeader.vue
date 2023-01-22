ffc6deccacdad595648b68e9ab92457adbb869a9
synw <synwx@protonmail.com>
2023-01-22 10:27:03 +0100

    Update header package

--- packages/@snowind-header/src/SwHeader.vue
+++ packages/@snowind-header/src/SwHeader.vue
@@ -7,33 +7,22 @@
       <div @click="back()">
         <slot name="mobile-back"></slot>
       </div>
-
       <slot name="mobile-branding"></slot>
     </div>
     <div class="flex-grow h-full" v-if="!isMobile">
       <slot name="menu"></slot>
     </div>
     <div class="flex items-center justify-end flex-grow h-full cursor-pointer" v-if="isMobile">
+      <slot name="extra-mobile-buttons"></slot>
       <div class="border-none btn" @click="onClick()">
         <slot name="menuicon">
           <div class="mr-5 text-3xl">
-            <svg
-              class="fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              aria-hidden="true"
-              focusable="false"
-              width="1em"
-              height="1em"
-              preserveAspectRatio="xMidYMid meet"
-              viewBox="0 0 24 24"
-            >
+            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+              aria-hidden="true" focusable="false" width="1em" height="1em" preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 24 24">
               <g>
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M2 6a2 2 0 0 1 2-2h16a2 2 0 1 1 0 4H4a2 2 0 0 1-2-2zm0 6a2 2 0 0 1 2-2h16a2 2 0 1 1 0 4H4a2 2 0 0 1-2-2zm0 6a2 2 0 0 1 2-2h16a2 2 0 1 1 0 4H4a2 2 0 0 1-2-2z"
-                />
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M2 6a2 2 0 0 1 2-2h16a2 2 0 1 1 0 4H4a2 2 0 0 1-2-2zm0 6a2 2 0 0 1 2-2h16a2 2 0 1 1 0 4H4a2 2 0 0 1-2-2zm0 6a2 2 0 0 1 2-2h16a2 2 0 1 1 0 4H4a2 2 0 0 1-2-2z" />
               </g>
             </svg>
           </div>
--- packages/@snowind-header/vite.config.ts
+++ packages/@snowind-header/vite.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import typescript2 from "rollup-plugin-typescript2"
-//import dts from 'vite-plugin-dts'
-const path = require('path')
+import path from 'path'
 
 export default defineConfig({
   plugins: [
-    typescript2({
-      check: false,
-      tsconfig: path.resolve(__dirname, 'tsconfig.json'),
-      clean: true
-    }),
     vue(),
-    //dts({ outputDir: path.resolve(__dirname, 'dist') }),
   ],
   build: {
     lib: {
